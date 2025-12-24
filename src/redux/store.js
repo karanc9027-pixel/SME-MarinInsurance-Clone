a80c63a6074d506820c56ec720b8ce4dd946e0d3
Karan Chavan <karanc9027@gmail.com>
2025-12-24 19:21:27 +0530

    Clean main branch

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import groupHealthReducer from "./slices/groupHealthSlice";
-
-export const store = configureStore( {
-    reducer: {
-        groupHealth: groupHealthReducer
-    }
-} );
